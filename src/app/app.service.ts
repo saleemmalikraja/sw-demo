3c46b30f113d11a67cda0327c264f87760cb3244
saleemmalikraja@gmail.com <saleemmalikraja@gmail.com>
2019-01-20 14:12:16 +0530

    added blog list component source code

--- src/app/app.service.ts
+++ src/app/app.service.ts
@@ -1,9 +1,70 @@
 import { Injectable } from '@angular/core';
-
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+import * as firebase from 'firebase';
+import firestore from 'firebase/firestore';
 @Injectable({
   providedIn: 'root'
 })
 export class AppService {
-
+  ref = firebase.firestore().collection('blogs');
   constructor() { }
+
+  getAllBlogs(): Observable<any> {
+    return new Observable((observer) => {
+      this.ref.onSnapshot((querySnapshot) => {
+        const blogs = [];
+        querySnapshot.forEach((doc) => {
+          const data = doc.data();
+          blogs.push({
+            key: doc.id,
+            title: data.title,
+            blog_url: data.blog_url,
+            author: data.author
+          });
+        });
+        observer.next(blogs);
+      });
+    });
+  }
+
+  getBlogDetailById(id: string): Observable<any> {
+    return new Observable((observer) => {
+      this.ref.doc(id).get().then((doc) => {
+        const data = doc.data();
+        observer.next({
+          key: doc.id,
+          title: data.title,
+          blog_url: data.blog_url,
+          author: data.author
+        });
+      });
+    });
+  }
+
+  postBlog(data): Observable<any> {
+    return new Observable((observer) => {
+      this.ref.add(data).then((doc) => {
+        observer.next({
+          key: doc.id,
+        });
+      });
+    });
+  }
+
+  updateBlogById(id: string, data): Observable<any> {
+    return new Observable((observer) => {
+      this.ref.doc(id).set(data).then(() => {
+        observer.next();
+      });
+    });
+  }
+
+  deleteBlog(id: string): Observable<{}> {
+    return new Observable((observer) => {
+      this.ref.doc(id).delete().then(() => {
+        observer.next();
+      });
+    });
+  }
 }
--- src/app/blog-list/blog-list.component.ts
+++ src/app/blog-list/blog-list.component.ts
@@ -1,15 +1,32 @@
 import { Component, OnInit } from '@angular/core';
-
+import { DataSource } from '@angular/cdk/collections';
+import { AppService } from '../app.service';
 @Component({
   selector: 'app-blog-list',
   templateUrl: './blog-list.component.html',
   styleUrls: ['./blog-list.component.css']
 })
 export class BlogListComponent implements OnInit {
+  displayedColumns = ['title', 'blog_url', 'author'];
+dataSource = new BlogDataSource(this.appService);
+  constructor(private appService: AppService) {}
 
-  constructor() { }
+  ngOnInit() {}
+}
 
-  ngOnInit() {
+
+
+export class BlogDataSource extends DataSource<any> {
+
+  constructor(private appService: AppService) {
+    super();
   }
 
+  connect() {
+    return this.appService.getAllBlogs();
+  }
+
+  disconnect() {
+
+  }
 }
